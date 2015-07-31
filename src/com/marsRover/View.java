0cdbe5e5628260cbb7a0b040a1af4b576c7cb5d4
dineshba <dineshb@thoughtworks.com>
2015-07-31 10:16:47 +0530

    Dinesh |001 | Display message to user

---
+++ src/com/marsRover/View.java
@@ -0,0 +1,8 @@
+package com.marsRover;
+
+public class View {
+
+    public void show(String message) {
+        System.out.println(message);
+    }
+}
---
+++ test/com/marsRover/ViewTest.java
@@ -0,0 +1,37 @@
+package com.marsRover;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class ViewTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    PrintStream original;
+
+    @Before
+    public void setUpStream() {
+        original = System.out;
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStream() {
+        System.setOut(original);
+
+    }
+
+    @Test
+    public void showTheMessageToUserToEnterTheNumberTheRover() {
+        View view = new View();
+
+        view.show("Enter the Number of the Rovers");
+
+        assertEquals("Enter the Number of the Rovers\n", outContent.toString());
+    }
+
+}
